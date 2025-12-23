fffaaafe74ca6d0c40d1cca28f3f9c8cae8b04ca
arabjanov <arabjanovxojiakbar@gmail.com>
2025-12-24 04:25:51 +0500

    Update Registerqr.jsx

--- src/qr-codes/Registerqr.jsx
+++ src/qr-codes/Registerqr.jsx
@@ -4,7 +4,7 @@ function Scanner() {
     <div style={{ textAlign: 'center', padding: '20px', fontFamily: 'Arial, sans-serif' }}>
       <h1 style={{ color: '#333' }}>QR Codeni Skaner Qiling!</h1>
       <img 
-        src="https://api.qrserver.com/v1/create-qr-code/?size=250x250&data=https://http://frontqr/login"
+        src="https://api.qrserver.com/v1/create-qr-code/?size=250x250&data=https://http://frontqr.vercel.app/login"
         alt="QR Code"
       />
     </div>
