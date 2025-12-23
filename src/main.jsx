af58fc6409b0156261f5c8a458224785e91e417a
arabjanov <arabjanovxojiakbar@gmail.com>
2025-12-24 01:48:37 +0500

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,14 +1,14 @@
 import { createRoot } from 'react-dom/client'
-import { HashRouter, Routes, Route } from 'react-router-dom'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import './index.css'
 import App from './App.jsx'
 import Salom from './Salom.jsx'
 
 createRoot(document.getElementById('root')).render(
-  <HashRouter>
+  <BrowserRouter>
     <Routes>
       <Route path='/' element={<App/>}/>
       <Route path='/salom' element={<Salom/>}/>
     </Routes>
-  </HashRouter>
+  </BrowserRouter>
 )
