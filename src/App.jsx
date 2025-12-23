4ef8d285e43b39c61d7e2ecb33713e8a567c6043
arabjanov <arabjanovxojiakbar@gmail.com>
2025-12-23 23:15:41 +0500

    Add redirect to /salom after delay on Home page
    Introduced useEffect with useNavigate to automatically redirect users from the Home component to /salom after 1 second. Also fixed the QR code image URL.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,21 @@
+import { useNavigate } from 'react-router-dom';
+import { useEffect } from 'react';
+
 function Home(){
+  const navigate = useNavigate();
+  useEffect( () => {
+    const time = setTimeout( () => { navigate('/salom')}, 1000 );
+    return clearTimeout( time );
+  }, [])
+
   return(
     <>
       <div style={{textAlign: "center"}}>
         <h1> QR codeni skaner qiling! </h1>
-        <img src="https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=https://frontqr/salom.vercel.app/salom"/>
+        <img src="https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=https://frontqr.vercel.app/salom"/>
       </div>
     </>
   )
-}
+};
 
 export default Home;
