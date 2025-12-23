b3c469af1598e43f0110248c0f75df33aab861e2
arabjanov <arabjanovxojiakbar@gmail.com>
2025-12-23 22:56:36 +0500

    Update QR code URL in Home component
    Modified the QR code image source to include '/salom' at the end of the URL for more specific QR code generation.

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ function Home(){
     <>
       <div style={{textAlign: "center"}}>
         <h1> QR codeni skaner qiling! </h1>
-        <img src="https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=https://frontqr/salom.vercel.app"/>
+        <img src="https://api.qrserver.com/v1/create-qr-code/?size=200x200&data=https://frontqr/salom.vercel.app/salom"/>
       </div>
     </>
   )
